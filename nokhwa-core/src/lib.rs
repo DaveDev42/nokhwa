ebee15dbe006d6534a86ee1bc35405628518072d
Dave Choi <dave.dev@icloud.com>
2026-04-13 03:41:23 +0900

    feat(core): introduce capability-based camera traits
    Replaces CaptureBackendTrait with four narrower traits:
    - CameraDevice (base: identity + controls)
    - FrameSource (streaming)
    - ShutterCapture (trigger + take_picture)
    - EventSource (take_events)
    
    Plus support types: EventPoll, CameraEvent.
    
    Downstream consumers (root nokhwa crate, backend bindings) are
    temporarily broken and will be migrated in subsequent commits
    (T7-T13). NOKHWA_SKIP_CLIPPY is used during the transition window;
    the workspace-wide clippy will return to green by T22.
    
    Locks the new surface via nokhwa-core/src/traits_tests.rs
    (failing-test-first scaffold from plan task 4).

--- nokhwa-core/src/lib.rs
+++ nokhwa-core/src/lib.rs
@@ -85,6 +85,8 @@ pub(crate) mod simd;
 #[doc(hidden)]
 pub use simd::bench_exports;
 pub mod traits;
+#[cfg(test)]
+mod traits_tests;
 pub mod types;
 
 #[cfg(feature = "wgpu-types")]
---
+++ nokhwa-core/src/traits_tests.rs
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2022 l1npengtul <l1npengtul@protonmail.com> / The Nokhwa Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![cfg(test)]
+
+use crate::buffer::Buffer;
+use crate::error::NokhwaError;
+use crate::traits::{
+    CameraDevice, CameraEvent, EventPoll, EventSource, FrameSource, ShutterCapture,
+};
+use crate::types::{
+    ApiBackend, CameraControl, CameraFormat, CameraInfo, ControlValueSetter, FrameFormat,
+    KnownCameraControl,
+};
+use std::borrow::Cow;
+use std::time::Duration;
+
+struct Dummy {
+    info: CameraInfo,
+    open: bool,
+}
+
+impl CameraDevice for Dummy {
+    fn backend(&self) -> ApiBackend {
+        ApiBackend::Browser
+    }
+    fn info(&self) -> &CameraInfo {
+        &self.info
+    }
+    fn controls(&self) -> Result<Vec<CameraControl>, NokhwaError> {
+        Ok(vec![])
+    }
+    fn set_control(
+        &mut self,
+        _id: KnownCameraControl,
+        _value: ControlValueSetter,
+    ) -> Result<(), NokhwaError> {
+        Ok(())
+    }
+}
+
+impl FrameSource for Dummy {
+    fn negotiated_format(&self) -> CameraFormat {
+        unimplemented!()
+    }
+    fn set_format(&mut self, _f: CameraFormat) -> Result<(), NokhwaError> {
+        Ok(())
+    }
+    fn compatible_formats(&mut self) -> Result<Vec<CameraFormat>, NokhwaError> {
+        Ok(vec![])
+    }
+    fn compatible_fourcc(&mut self) -> Result<Vec<FrameFormat>, NokhwaError> {
+        Ok(vec![])
+    }
+    fn open(&mut self) -> Result<(), NokhwaError> {
+        self.open = true;
+        Ok(())
+    }
+    fn is_open(&self) -> bool {
+        self.open
+    }
+    fn frame(&mut self) -> Result<Buffer, NokhwaError> {
+        unimplemented!()
+    }
+    fn frame_raw(&mut self) -> Result<Cow<'_, [u8]>, NokhwaError> {
+        Ok(Cow::Borrowed(&[]))
+    }
+    fn close(&mut self) -> Result<(), NokhwaError> {
+        self.open = false;
+        Ok(())
+    }
+}
+
+impl ShutterCapture for Dummy {
+    fn trigger(&mut self) -> Result<(), NokhwaError> {
+        Ok(())
+    }
+    fn take_picture(&mut self, _timeout: Duration) -> Result<Buffer, NokhwaError> {
+        Err(NokhwaError::TimeoutError(Duration::ZERO))
+    }
+}
+
+struct DummyEvents;
+impl EventPoll for DummyEvents {
+    fn try_next(&mut self) -> Option<CameraEvent> {
+        None
+    }
+    fn next_timeout(&mut self, _d: Duration) -> Option<CameraEvent> {
+        None
+    }
+}
+
+impl EventSource for Dummy {
+    fn take_events(&mut self) -> Result<Box<dyn EventPoll + Send>, NokhwaError> {
+        Ok(Box::new(DummyEvents))
+    }
+}
+
+fn sample_info() -> CameraInfo {
+    use crate::types::CameraIndex;
+    CameraInfo::new("dummy", "dummy", "", CameraIndex::Index(0))
+}
+
+#[test]
+fn dummy_implements_all_capabilities() {
+    let mut d = Dummy {
+        info: sample_info(),
+        open: false,
+    };
+    let _: &dyn CameraDevice = &d;
+    let _: &mut dyn FrameSource = &mut d;
+    let _: &mut dyn ShutterCapture = &mut d;
+    let _: &mut dyn EventSource = &mut d;
+}
+
+#[test]
+fn shutter_capture_default_methods() {
+    let mut d = Dummy {
+        info: sample_info(),
+        open: false,
+    };
+    assert!(d.lock().is_ok());
+    assert!(d.unlock().is_ok());
+    let r = d.capture(Duration::ZERO);
+    assert!(matches!(r, Err(NokhwaError::TimeoutError(_))));
+}
